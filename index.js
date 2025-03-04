8f1c6bc81ef4f9fee70b6ae54e59e3b4fafdc2a9
Sanhil Rai <sanhil.rai@viamagus.com>
2025-03-04 12:21:06 +0530

    feat: update port number

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+    console.log('express app listening on port 3000!!!!!!!!!!');
 })
